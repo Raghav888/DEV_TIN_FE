1a1459de2790afb122cb600eeb44327e3149ae61
raghav888 <raghavbang2012@gmail.com>
2025-10-25 11:39:26 +0530

    feat: integrate Tailwind CSS and DaisyUI for styling
    - Added Tailwind CSS and DaisyUI dependencies in package.json.
    - Updated App component to include a responsive navbar with user profile dropdown.
    - Refactored index.css to utilize Tailwind CSS and DaisyUI, removing custom styles.
    - Configured Vite to use Tailwind CSS and DaisyUI plugins.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import daisyui from "daisyui";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tailwindcss({
+      plugins: [daisyui],
+      daisyui: {
+        theme: ["dark"], // or ["light", "dark"]
+      },
+    }),
+    react(),
+  ],
+});
