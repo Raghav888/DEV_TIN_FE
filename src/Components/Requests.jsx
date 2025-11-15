3893b4d798905bc84e8ac31ff9f79e08d2127d38
raghav888 <raghavbang2012@gmail.com>
2025-11-15 22:27:53 +0530

    feat: add request handling functionality with accept and reject actions

--- src/Components/Requests.jsx
+++ src/Components/Requests.jsx
@@ -2,13 +2,13 @@ import axios from "axios";
 import { useEffect } from "react";
 import { BASE_URL } from "../constants/url";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../utils/requestSlice";
+import { addRequests, removeRequest } from "../utils/requestSlice";
 
 export const Requests = () => {
   const dispatch = useDispatch();
   const requestData = useSelector((state) => state.requests);
 
-  const handleRequest = async () => {
+  const fetchRequest = async () => {
     try {
       const response = await axios.get(BASE_URL + "/user/requests/received", {
         withCredentials: true,
@@ -20,8 +20,23 @@ export const Requests = () => {
     }
   };
 
+  const handleRequest = async (user, action) => {
+    try {
+      await axios.post(
+        BASE_URL + "/requests/review/" + action + "/" + user._id,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeRequest(user._id));
+    } catch (err) {
+      console.error("Error handling request:", err);
+    }
+  };
+
   useEffect(() => {
-    handleRequest();
+    fetchRequest();
   }, []);
 
   if (!requestData) {
@@ -38,31 +53,45 @@ export const Requests = () => {
 
   return (
     <div className="text-center mb-20">
-      <h1 className="my-2 text-2xl">Requests</h1>
-      {requestData.map((request) => (
-        <div
-          key={request._id}
-          className="card card-bordered bg-base-300 w-96 mx-auto my-4"
-        >
-          <div className="card-body">
-            <h2 className="card-title">
-              {request.firstName} {request.lastName}
-            </h2>
-            <img
-              src={request.photoUrl}
-              alt="Profile"
-              className="w-24 h-24 rounded-full mx-auto"
-            />
-            <p>Age: {request.age}</p>
-            <p>Gender: {request.gender}</p>
-            <p>About: {request.about}</p>
-            <div className="card-actions justify-center ">
-              <button className="btn btn-primary mr-2">Reject</button>
-              <button className="btn btn-secondary">Accept</button>
+      <h1 className="my-2 text-2xl">Connection Requests</h1>
+      {requestData.map((request) => {
+        const { firstName, lastName, age, photoUrl, gender, about } =
+          request.requestFromId;
+        return (
+          <div
+            key={request._id}
+            className="card card-bordered bg-base-300 w-96 mx-auto my-4"
+          >
+            <div className="card-body">
+              <h2 className="card-title">
+                {firstName} {lastName}
+              </h2>
+              <img
+                src={photoUrl}
+                alt="Profile"
+                className="w-24 h-24 rounded-full mx-auto"
+              />
+              <p>Age: {age}</p>
+              <p>Gender: {gender}</p>
+              <p>About: {about}</p>
+              <div className="card-actions justify-center ">
+                <button
+                  className="btn btn-primary mr-2"
+                  onClick={() => handleRequest(request, "rejected")}
+                >
+                  Reject
+                </button>
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => handleRequest(request, "accepted")}
+                >
+                  Accept
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
--- src/utils/requestSlice.js
+++ src/utils/requestSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
 const requestSlice = createSlice({
     name: 'requests',
     initialState: null,
@@ -6,8 +7,11 @@ const requestSlice = createSlice({
         addRequests: (state, action) => {
             return action.payload;
         },
+        removeRequest: (state, action) => {
+            return state.filter(request => request._id !== action.payload);
+        }
     }
 });
 
-export const { addRequests } = requestSlice.actions;
+export const { addRequests, removeRequest } = requestSlice.actions;
 export default requestSlice.reducer;
